493c9c25ca20ace132e3da3a0ffa07fb73da6943
Tommy Trøen <tommy.troen@nav.no>
2023-05-26 09:48:28 +0200

    feat: add fqdn policy for proxy.golang.org, remove ingress netpols
    Co-authored-by: ybelmekk <youssef.bel.mekki@nav.no>
    Co-authored-by: sindrerh2 <sindre.rodseth.hansen@nav.no>
    Co-authored-by: muni10 <sten.ivar.rokke@nav.no>

--- pkg/client/client_integration_test.go
+++ pkg/client/client_integration_test.go
@@ -51,6 +51,7 @@ func TestIntegration(t *testing.T) {
 	team, e := c.GetTeam(ctx, "Administrators")
 	if e != nil {
 		log.Fatalf("get team uuid: %v", e)
+
 	}
 
 	t.Run("Get version", func(t *testing.T) {
